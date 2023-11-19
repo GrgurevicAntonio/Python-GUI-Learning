5113d2559c525ec2cc7a9f1e2c2149df6d06428b
AntonioGrgurevic <antoniogrgurevic1802@gmail.com>
2023-11-19 11:27:15 +0100

    Nove promjene

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from tkinter import *
 
 window = Tk()
 
-photo = PhotoImage(file="icona.png")
+window.config(background="black")
+
+icona = PhotoImage(file="icona.png")
 
 label = Label(window,
               text="Hello World",
@@ -16,10 +18,80 @@ label = Label(window,
               bd=10,
               padx=20,
               pady=20,
-              image=photo,
+              image=icona,
               compound="bottom")
 
-window.config(background="black")
+count = 0
+
+
+def click():
+
+    global count
+    count += 1
+    print(count)
+
+
+like = PhotoImage(file="like.png")
+
+button = Button(window,
+                text="Click me",
+                command=click,
+                font=("Comic Sans", 30),
+                fg="red",
+                bg="black",
+                activeforeground="black",
+                activebackground="black",
+                image=like,
+                compound="top")
+
+entry = Entry(window,
+              font=("Arial", 30),
+              fg="dark red",
+              bg="silver")
+
+password = Entry(window,
+                 font=("Arial", 30),
+                 fg="dark red",
+                 bg="silver",
+                 show="*")
+
+
+def submit():
+    username = entry.get()
+    print("Hello " + username)
+    entry.config(state=DISABLED)
+
+
+def delete():
+    entry.delete(0, END)
+
+
+def backspace():
+    entry.delete(len(entry.get()) - 1, END)
+
+
+submit_btn1 = Button(window,
+                     text="Submit",
+                     command=submit)
+
+
+delete_btn1 = Button(window,
+                     text="Delete",
+                     command=delete)
+
+
+backspace_btn1 = Button(window,
+                        text="Backspace",
+                        command=backspace)
+
+
 label.pack()
+button.pack()
+entry.pack(side=LEFT)
+submit_btn1.pack(side=RIGHT)
+delete_btn1.pack(side=RIGHT)
+backspace_btn1.pack(side=RIGHT)
+password.pack()
+
 
 window.mainloop()
